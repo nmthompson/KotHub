15dcf32b3e43719b04d49016fce51fab1233e9ad
Joe DeCapo <joe@polka.cat>
2017-11-23 10:45:51 -0600

    Add basic get request example

--- src/main/kotlin/KotHub/KotHub.kt
+++ src/main/kotlin/KotHub/KotHub.kt
@@ -4,6 +4,9 @@
 
 package KotHub
 
+import com.github.kittinunf.fuel.core.FuelManager
+import com.github.kittinunf.fuel.httpGet
+
 fun getGreeting(): String {
     val words = mutableListOf<String>()
     words.add("Hello,")
@@ -14,4 +17,19 @@ fun getGreeting(): String {
 
 fun main(args: Array<String>) {
     println(getGreeting())
+    get()
+}
+
+fun get() {
+    FuelManager.instance.basePath = "http://httpbin.org"
+    "/get".httpGet().responseString { request, response, result ->
+        //make a GET to http://httpbin.org/get and do something with response
+        val (data, error) = result
+        if (error == null) {
+            //do something when success
+            println(data)
+        } else {
+            //error handling
+        }
+    }
 }
