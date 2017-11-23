3673c48d769f7c7dcd80d02b874c5f05849c744d
Nick Thompson <thompson.nick.m@gmail.com>
2017-11-23 14:20:23 -0600

    Opt in for null on all model variables

--- src/main/kotlin/com/KotHub/models/KMilestone.kt
+++ src/main/kotlin/com/KotHub/models/KMilestone.kt
@@ -1,5 +1,5 @@
 package com.KotHub.models
 
-data class KMilestone(val url: String, val htmlUrl: String, val labelsUrl: String, val id: Int, val number: Int,
-                      val state: String, val title: String, val description: String, val creator: KUser, val openIssues: Int,
-                      val closedIssues: Int, val createdAt: String, val updatedAt: String, val closedAt: String, val dueOn: String)
+data class KMilestone(val url: String?, val htmlUrl: String?, val labelsUrl: String?, val id: Int?, val number: Int?,
+                      val state: String?, val title: String?, val description: String?, val creator: KUser?, val openIssues: Int?,
+                      val closedIssues: Int?, val createdAt: String?, val updatedAt: String?, val closedAt: String?, val dueOn: String?)
--- src/main/kotlin/com/KotHub/models/KPermissions.kt
+++ src/main/kotlin/com/KotHub/models/KPermissions.kt
@@ -1,3 +1,3 @@
 package com.KotHub.models
 
-data class KPermissions(val admin: Boolean, val push: Boolean, val pull: Boolean)
+data class KPermissions(val admin: Boolean?, val push: Boolean?, val pull: Boolean?)
