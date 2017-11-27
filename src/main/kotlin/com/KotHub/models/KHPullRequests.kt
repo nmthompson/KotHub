014267e6b62701981f7a8fcc962ea2c15277b5ec
Nick Thompson <thompson.nick.m@gmail.com>
2017-11-27 14:29:31 -0600

    Rename Pull Requests model to meet naming convention

---
+++ src/main/kotlin/com/KotHub/models/KHPullRequests.kt
@@ -0,0 +1,6 @@
+package com.KotHub.models
+
+data class KHPullRequests(val url: String?,
+                          val htmlUrl: String?,
+                          val diffUrl: String?,
+                          val patchUrl: String?)
--- src/main/kotlin/com/KotHub/models/KPullRequests.kt
+++
@@ -1,6 +0,0 @@
-package com.KotHub.models
-
-data class KPullRequests(val url: String?,
-                         val htmlUrl: String?,
-                         val diffUrl: String?,
-                         val patchUrl: String?)
